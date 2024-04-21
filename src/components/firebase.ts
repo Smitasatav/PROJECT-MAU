59bb3506248b57847fd310ca2e1358b3ac891a74
Ayushi0612 <124232340+Ayushi0612@users.noreply.github.com>
2024-04-21 16:22:29 +0530

    firebase added

---
+++ src/components/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBLxkJ2xrbXVk1jrXFoF0_okHpvpL6PVFg",
+  authDomain: "project-mau-227ed.firebaseapp.com",
+  projectId: "project-mau-227ed",
+  storageBucket: "project-mau-227ed.appspot.com",
+  messagingSenderId: "3574141911",
+  appId: "1:3574141911:web:8f3ff8083f4f3941cdb192"
+};
+
+export const app = initializeApp(firebaseConfig);
